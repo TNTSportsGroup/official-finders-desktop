69fadad58d4d1dc442e6c2ccc11646277757ff3b
Camilo Guerrero <camiloguerrero@Phils-Mac-mini.local>
2019-02-20 10:22:37 -0600

    initial commit used the react forge boilerplate and upgraded some packages

---
+++ src/app.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import {Button} from 'antd';
+
+
+export class App extends React.Component<undefined, undefined> {
+  render() {
+    return (
+      <div>
+        <h2>Naniii</h2>
+        <Button type="primary">Primary</Button>
+      </div>
+    );
+  }
+}
