03137acd3e51ab097baf4f65810f961b993c12e8
Camilo Guerrero <camiloguerrero@Phils-Mac-mini.local>
2019-02-20 11:18:43 -0600

    updated our Button with it's proper title and an icon with the arrow right

--- src/modules/Home/Home.tsx
+++ src/modules/Home/Home.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 // import {Link} from 'react-router-dom';
 
-import {Button, Menu, Icon} from 'antd';
+import {Button, Icon} from 'antd';
 import styled from 'styled-components';
 // import { routes } from '../constants/routes';
 
@@ -17,7 +17,9 @@ export const Home = () => (
     <div style={{height: '100%', width: '100%', minHeight: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', backgroundImage: 'linear-gradient(45deg, rgba(0, 216, 255, 0.5) 10%, rgba(0, 1, 127, 0.7))'}}>
     <div style={{ padding: 10, color: "white", display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start'}}>
         <Header>Home</Header>
-        <Button type='primary'>Btn</Button>
+        <Button type='primary'>HWRP
+            <Icon type="arrow-right"/>
+        </Button>
         
     
     
