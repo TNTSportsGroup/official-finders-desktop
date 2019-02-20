6f3d6abbd944866e7b8c4b9ba1bf1c2d12ad1b6c
Camilo Guerrero <camiloguerrero@Phils-Mac-mini.local>
2019-02-20 12:09:54 -0600

    used our Nav component and updated our ui

--- src/modules/Hwrp/Hwrp.tsx
+++ src/modules/Hwrp/Hwrp.tsx
@@ -1,14 +1,30 @@
 import * as React from 'react';
 import {Link} from 'react-router-dom';
-import {Upload} from 'antd';
+import {Upload, Icon, Button} from 'antd';
+import { Nav } from '../components/Nav';
+import { routes } from '../constants/routes';
+
+
 
 export class Hwrp extends React.Component<any, any> {
 
 
     render() {
         return (
-            <div>
-                hi
+            <div style={{ minHeight: '100vh', justifyContent: 'flex-start', flexDirection: 'column'}}>
+                <Nav>
+                    <Link to={routes.HOME}>
+                    <Icon type="arrow-left" style={{color: 'white', marginLeft: '.5rem', fontSize: '1.5rem'}}/>
+                    </Link>
+                    
+                </Nav>
+                <div style={{padding: 15, display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
+                    <Upload>
+                    <Button type="primary">
+                        <Icon type="upload" /> Click to Upload
+                        </Button>
+                    </Upload>
+                </div>
             </div>
         )
     }
