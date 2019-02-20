f7f71ad75b46b2e949cf717f1181ccf781942366
Camilo Guerrero <camiloguerrero@Phils-Mac-mini.local>
2019-02-20 11:16:02 -0600

    updated our styles

--- src/modules/Home/Home.tsx
+++ src/modules/Home/Home.tsx
@@ -5,10 +5,7 @@ import {Button, Menu, Icon} from 'antd';
 import styled from 'styled-components';
 // import { routes } from '../constants/routes';
 
-const Btn = styled.button`
-    color: white
 
-`;
 
 const Header = styled.h1`
     color: white;
@@ -18,10 +15,10 @@ const Header = styled.h1`
 
 export const Home = () => (
     <div style={{height: '100%', width: '100%', minHeight: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', backgroundImage: 'linear-gradient(45deg, rgba(0, 216, 255, 0.5) 10%, rgba(0, 1, 127, 0.7))'}}>
-    <div style={{ padding: 10, color: "white", display: 'flex', flexDirection: 'column', justifyContent: 'flex-start'}}>
+    <div style={{ padding: 10, color: "white", display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start'}}>
         <Header>Home</Header>
         <Button type='primary'>Btn</Button>
-        <Btn>Btn</Btn>
+        
     
     
     </div>
