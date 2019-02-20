e7451a17feefeee7eacc89ffb8b11a3b1f44afab
Camilo Guerrero <camiloguerrero@Phils-Mac-mini.local>
2019-02-20 12:09:34 -0600

    created a basic Nav component that willbe used a nav bar for screens.

---
+++ src/modules/components/Nav.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+
+export const Nav = styled.div`
+    height: 2.5rem;
+    background-color: rgb(34,39,43);
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    
+`
