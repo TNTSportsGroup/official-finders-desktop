05ecb95bc1f7d5d903684e54b8369a66a04f339c
Camilo Guerrero <camiloguerrero@Phils-Mac-mini.local>
2019-02-21 13:09:22 -0600

    set up more properties in our internal state. This will help us set up the manual upload and have access tot he response object sent by the server

--- src/modules/Hwrp/Hwrp.tsx
+++ src/modules/Hwrp/Hwrp.tsx
@@ -10,15 +10,25 @@ import { UploadChangeParam } from 'antd/lib/upload/interface';
 export class Hwrp extends React.Component<any, any> {
     state = {
         disabled: false,
-        data: {}
+        data: {},
+        error: {},
+        fileName: '',
+        fileList: [],
+        uploading: false
     }
 
     handleChange = (info: UploadChangeParam) => {
             console.log(info.file);
     }
 
+    handleUpload = () => {
+        const {fileList} = this.state;
+    }
+
 
     render() {
+
+        const {fileList, uploading} = this.state;
         return (
             <div style={{ minHeight: '100vh', justifyContent: 'flex-start', flexDirection: 'column'}}>
                 <Nav>
@@ -27,12 +37,22 @@ export class Hwrp extends React.Component<any, any> {
                     </Link>
                 
                 </Nav>
-                <div style={{padding: 15, display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
+                <div style={{padding: '5rem', display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
                     <Upload onChange={this.handleChange} name="file" accept="*" action="http://localhost:3000/hwrp" multiple={false}>
                     <Button type="primary" disabled={this.state.disabled}>
-                        <Icon type="upload" /> Click to Upload
+                        <Icon type="upload" /> Select File
                         </Button>
                     </Upload>
+
+                    <Button
+                        type="primary"
+                        onClick={this.handleUpload}
+                        disabled={fileList.length === 0}
+                        loading={uploading}
+                        style={{ marginTop: 16 }}
+                        >
+                        {uploading ? 'Uploading' : 'Start Upload' }
+                    </Button>
                 </div>
             </div>
         )
