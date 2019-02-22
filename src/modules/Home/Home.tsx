2bba4c2b663332be165c0c7c4517d2fb19ee8144
datdevboi <camiloguerrero@Phils-Mac-mini.local>
2019-02-22 16:17:51 -0600

    formatted with prettier

--- src/modules/Home/Home.tsx
+++ src/modules/Home/Home.tsx
@@ -1,36 +1,45 @@
-import * as React from 'react';
-import {Link} from 'react-router-dom';
+import * as React from "react";
+import { Link } from "react-router-dom";
 
-import {Button, Icon} from 'antd';
-import styled from 'styled-components';
-import { routes } from '../constants/routes';
-// import { routes } from '../constants/routes';
-
-
+import { Button, Icon } from "antd";
+import styled from "styled-components";
+import { routes } from "../constants/routes";
 
 const Header = styled.h1`
-    color: white;
-    font-size: 45px;
-`
-
+  color: white;
+  font-size: 45px;
+`;
 
 export const Home = () => (
-    <div style={{height: '100%', width: '100%', minHeight: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', backgroundImage: 'linear-gradient(45deg, rgba(0, 216, 255, 0.5) 10%, rgba(0, 1, 127, 0.7))'}}>
-    <div style={{ padding: 10, color: "white", display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start'}}>
-        <Header>Home</Header>
-        <Link to={routes.HWRPAYROLL}>
-        <Button type='primary'>HWRP
-            <Icon type="arrow-right"/>
+  <div
+    style={{
+      height: "100%",
+      width: "100%",
+      minHeight: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      backgroundImage:
+        "linear-gradient(45deg, rgba(0, 216, 255, 0.5) 10%, rgba(0, 1, 127, 0.7))"
+    }}
+  >
+    <div
+      style={{
+        padding: 10,
+        color: "white",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        alignItems: "flex-start"
+      }}
+    >
+      <Header>Home</Header>
+      <Link to={routes.HWRPAYROLL}>
+        <Button type="primary">
+          HWRP
+          <Icon type="arrow-right" />
         </Button>
-
-        </Link>
-        
-        
-    
-    
+      </Link>
     </div>
-    </div>
-    
-    
-    
-)
+  </div>
+);
