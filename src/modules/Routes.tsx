b21c79fff78dfbed7fc579dda1507572badc6efc
datdevboi <cami172013@gmail.com>
2019-02-26 11:33:06 -0600

    created a Route for HwrInvoice

--- src/modules/Routes.tsx
+++ src/modules/Routes.tsx
@@ -3,10 +3,12 @@ import { Route } from "react-router-dom";
 import { routes } from "./constants/routes";
 import { Home } from "./Home/Home";
 import { Hwrp } from "./Hwrp/Hwrp";
+import { HwrInvoice } from "./HwrInvoice/HwrInvoice";
 
 export const Routes = () => (
   <>
     <Route exac={true} path={routes.HWRPAYROLL} component={Hwrp} />
+    <Route exac={true} path={routes.HWRINVOICE} component={HwrInvoice} />
     <Route exact={true} path={routes.HOME} component={Home} />
   </>
 );
