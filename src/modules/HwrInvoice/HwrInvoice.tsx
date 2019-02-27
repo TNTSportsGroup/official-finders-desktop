c8bcb8c91d5c436632f1bd6a701e569128d7350a
datdevboi <cami172013@gmail.com>
2019-02-27 12:39:49 -0600

    Removed some styles and removed the commented code.

--- src/modules/HwrInvoice/HwrInvoice.tsx
+++ src/modules/HwrInvoice/HwrInvoice.tsx
@@ -39,7 +39,6 @@ export class HwrInvoice extends React.Component {
         style={{
           height: "100%",
           width: "100%",
-          backgroundColor: "pink",
           justifyContent: "flex-start",
           flexDirection: "column"
         }}
@@ -80,72 +79,63 @@ export class HwrInvoice extends React.Component {
         </div>
 
         <Layout hasSider={true} style={{ height: "100%" }}>
-          <Sider>Sider</Sider>
+          <Sider>
+            <Menu
+              theme="dark"
+              onClick={() => console.log("click")}
+              style={{ width: 256, height: "100%" }}
+              defaultOpenKeys={["sub1"]}
+              selectedKeys={["0"]}
+              mode="inline"
+            >
+              <SubMenu
+                key="sub1"
+                title={
+                  <span>
+                    <Icon type="mail" />
+                    <span>Navigation One</span>
+                  </span>
+                }
+              >
+                <Menu.Item key="1">Option 1</Menu.Item>
+                <Menu.Item key="2">Option 2</Menu.Item>
+                <Menu.Item key="3">Option 3</Menu.Item>
+                <Menu.Item key="4">Option 4</Menu.Item>
+              </SubMenu>
+              <SubMenu
+                key="sub2"
+                title={
+                  <span>
+                    <Icon type="appstore" />
+                    <span>Navigtion Two</span>
+                  </span>
+                }
+              >
+                <Menu.Item key="5">Option 5</Menu.Item>
+                <Menu.Item key="6">Option 6</Menu.Item>
+                <SubMenu key="sub3" title="Submenu">
+                  <Menu.Item key="7">Option 7</Menu.Item>
+                  <Menu.Item key="8">Option 8</Menu.Item>
+                </SubMenu>
+              </SubMenu>
+              <SubMenu
+                key="sub4"
+                title={
+                  <span>
+                    <Icon type="setting" />
+                    <span>Navigation Three</span>
+                  </span>
+                }
+              >
+                <Menu.Item key="9">Option 9</Menu.Item>
+                <Menu.Item key="10">Option 10</Menu.Item>
+                <Menu.Item key="11">Option 11</Menu.Item>
+                <Menu.Item key="12">Option 12</Menu.Item>
+              </SubMenu>
+            </Menu>
+          </Sider>
           <Content>Content</Content>
         </Layout>
-
-        {/* <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            backgroundColor: "blue",
-            flex: 1
-          }}
-        >
-          <Menu
-            theme="dark"
-            onClick={() => console.log("click")}
-            style={{ width: 256, height: "100%" }}
-            defaultOpenKeys={["sub1"]}
-            selectedKeys={["0"]}
-            mode="inline"
-          >
-            <SubMenu
-              key="sub1"
-              title={
-                <span>
-                  <Icon type="mail" />
-                  <span>Navigation One</span>
-                </span>
-              }
-            >
-              <Menu.Item key="1">Option 1</Menu.Item>
-              <Menu.Item key="2">Option 2</Menu.Item>
-              <Menu.Item key="3">Option 3</Menu.Item>
-              <Menu.Item key="4">Option 4</Menu.Item>
-            </SubMenu>
-            <SubMenu
-              key="sub2"
-              title={
-                <span>
-                  <Icon type="appstore" />
-                  <span>Navigtion Two</span>
-                </span>
-              }
-            >
-              <Menu.Item key="5">Option 5</Menu.Item>
-              <Menu.Item key="6">Option 6</Menu.Item>
-              <SubMenu key="sub3" title="Submenu">
-                <Menu.Item key="7">Option 7</Menu.Item>
-                <Menu.Item key="8">Option 8</Menu.Item>
-              </SubMenu>
-            </SubMenu>
-            <SubMenu
-              key="sub4"
-              title={
-                <span>
-                  <Icon type="setting" />
-                  <span>Navigation Three</span>
-                </span>
-              }
-            >
-              <Menu.Item key="9">Option 9</Menu.Item>
-              <Menu.Item key="10">Option 10</Menu.Item>
-              <Menu.Item key="11">Option 11</Menu.Item>
-              <Menu.Item key="12">Option 12</Menu.Item>
-            </SubMenu>
-          </Menu>
-        </div> */}
       </div>
     );
   }
