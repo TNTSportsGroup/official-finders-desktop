aba3afa75b55e726bf9ddb7c05f65511f3c5c2c7
datdevboi <cami172013@gmail.com>
2019-02-27 10:23:52 -0600

    Updated our Header component. Added an interface and made some styles be adaptable by props.

--- src/modules/components/Header.tsx
+++ src/modules/components/Header.tsx
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 
-export const Header = styled.h1`
-  color: white;
-  font-size: 45px;
+interface IHeaderProps {
+  color?: string;
+  fontSize?: string;
+}
+
+export const Header = styled.h1<IHeaderProps>`
+  color: ${({ color }) => (color ? color : "white")};
+  font-size: ${({ fontSize }) => (fontSize ? fontSize : "45px")};
 `;
