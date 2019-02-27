583947846f32beb1801f6a918f6fa85adcae692f
datdevboi <cami172013@gmail.com>
2019-02-27 11:56:10 -0600

    Made the proper imports  and made a quick Menu component.

--- src/modules/HwrInvoice/HwrInvoice.tsx
+++ src/modules/HwrInvoice/HwrInvoice.tsx
@@ -2,7 +2,10 @@ import * as React from "react";
 import { Nav } from "../components/Nav";
 import { Link } from "react-router-dom";
 import { routes } from "../constants/routes";
-import { Icon, Upload, Button } from "antd";
+import { Icon, Upload, Button, Menu } from "antd";
+import console = require("console");
+
+const SubMenu = Menu.SubMenu;
 
 export class HwrInvoice extends React.Component {
   state = {
@@ -72,6 +75,61 @@ export class HwrInvoice extends React.Component {
               <Icon type="upload" /> Upload Invoice
             </Button>
           </Upload>
+        </div>
+        <div>
+          <Menu
+            theme="dark"
+            onClick={() => console.log("click")}
+            style={{ width: 256, height: "100%" }}
+            defaultOpenKeys={["sub1"]}
+            selectedKeys={["0"]}
+            mode="inline"
+          >
+            <SubMenu
+              key="sub1"
+              title={
+                <span>
+                  <Icon type="mail" />
+                  <span>Navigation One</span>
+                </span>
+              }
+            >
+              <Menu.Item key="1">Option 1</Menu.Item>
+              <Menu.Item key="2">Option 2</Menu.Item>
+              <Menu.Item key="3">Option 3</Menu.Item>
+              <Menu.Item key="4">Option 4</Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key="sub2"
+              title={
+                <span>
+                  <Icon type="appstore" />
+                  <span>Navigtion Two</span>
+                </span>
+              }
+            >
+              <Menu.Item key="5">Option 5</Menu.Item>
+              <Menu.Item key="6">Option 6</Menu.Item>
+              <SubMenu key="sub3" title="Submenu">
+                <Menu.Item key="7">Option 7</Menu.Item>
+                <Menu.Item key="8">Option 8</Menu.Item>
+              </SubMenu>
+            </SubMenu>
+            <SubMenu
+              key="sub4"
+              title={
+                <span>
+                  <Icon type="setting" />
+                  <span>Navigation Three</span>
+                </span>
+              }
+            >
+              <Menu.Item key="9">Option 9</Menu.Item>
+              <Menu.Item key="10">Option 10</Menu.Item>
+              <Menu.Item key="11">Option 11</Menu.Item>
+              <Menu.Item key="12">Option 12</Menu.Item>
+            </SubMenu>
+          </Menu>
         </div>
       </div>
     );
