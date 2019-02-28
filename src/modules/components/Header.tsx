ab6f6b2a5cda49cf67c368c4c7fab127b2df920c
datdevboi <cami172013@gmail.com>
2019-02-28 14:05:25 -0600

    updated our Header styles and props

--- src/modules/components/Header.tsx
+++ src/modules/components/Header.tsx
@@ -3,9 +3,11 @@ import styled from "styled-components";
 interface IHeaderProps {
   color?: string;
   fontSize?: string;
+  fontWeight?: string;
 }
 
 export const Header = styled.h1<IHeaderProps>`
   color: ${({ color }) => (color ? color : "white")};
-  font-size: ${({ fontSize }) => (fontSize ? fontSize : "45px")};
+  font-size: ${({ fontSize }) => (fontSize ? fontSize : "2rem")};
+  font-weight: ${({ fontWeight }) => (fontWeight ? fontWeight : "40")};
 `;
