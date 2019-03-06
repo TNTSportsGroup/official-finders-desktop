e973382df8392ab6df3fa779408215434a11cc30
datdevboi <cami172013@gmail.com>
2019-03-06 12:59:25 -0600

    fixed imports and uncommented routes

--- src/modules/Routes.tsx
+++ src/modules/Routes.tsx
@@ -2,13 +2,13 @@ import * as React from "react";
 import { Route } from "react-router-dom";
 import { routes } from "./constants/routes";
 import { Home } from "./Home/Home";
-import { Hwrp } from "./Hwrp/Hwrp";
 import { HwrInvoice } from "./HwrInvoice/HwrInvoice";
+import { Hwrp } from "./Hwrp/Hwrp";
 
 export const Routes = () => (
   <>
-    {/* <Route exac={true} path={routes.HWRPAYROLL} component={Hwrp} /> */}
-    <Route exac={true} path={routes.HOME} component={HwrInvoice} />
-    {/* <Route exact={true} path={routes.HOME} component={Home} /> */}
+    <Route exac={true} path={routes.HWRPAYROLL} component={Hwrp} />
+    <Route exac={true} path={routes.HWRINVOICE} component={HwrInvoice} />
+    <Route exact={true} path={routes.HOME} component={Home} />
   </>
 );
