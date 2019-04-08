595557eb2d49e9f57521b8bb460937e9af9a402e
datdevboi <cami172013@gmail.com>
2019-04-08 15:15:43 -0500

    Updated our class component to functional component
    Introduced the use of hooks in order to have state management.

--- src/modules/QuickScoresGames/QuickScoresGames.tsx
+++ src/modules/QuickScoresGames/QuickScoresGames.tsx
@@ -4,69 +4,65 @@ import { Link } from "react-router-dom";
 import { routes } from "../constants/routes";
 import { Icon, Button, Alert, Table } from "antd";
 
-export class QuickScoresGames extends React.Component {
-  state = {
-    error: ""
-  };
-  render() {
-    return (
-      <div style={{ height: "100%", display: "flex", flexDirection: "column" }}>
-        <Nav>
-          <Link to={routes.HOME}>
-            <Icon
-              type="arrow-left"
-              style={{
-                color: "white",
-                marginLeft: ".5rem",
-                fontSize: "1.5rem"
-              }}
-            />
-          </Link>
-        </Nav>
+export const QuickScoresGames = () => {
+  const [error, setError] = React.useState("ff");
+  return (
+    <div style={{ height: "100%", display: "flex", flexDirection: "column" }}>
+      <Nav>
+        <Link to={routes.HOME}>
+          <Icon
+            type="arrow-left"
+            style={{
+              color: "white",
+              marginLeft: ".5rem",
+              fontSize: "1.5rem"
+            }}
+          />
+        </Link>
+      </Nav>
+      <div
+        style={{
+          flex: 1,
+          display: "flex",
+          flexDirection: "column"
+        }}
+      >
         <div
           style={{
-            flex: 1,
+            flex: 1.5,
             display: "flex",
-            flexDirection: "column"
+            justifyContent: "space-around",
+            alignItems: "center",
+            flexDirection: "row",
+            borderBottomStyle: "solid",
+            borderBottomColor: "grey",
+            borderBottomWidth: "1px"
           }}
         >
+          <Button type="primary">Get Games</Button>
+          {error && (
+            <Alert
+              type="error"
+              message=""
+              showIcon={true}
+              description={error}
+              closable={true}
+            />
+          )}
+        </div>
+        <div style={{ flex: 11, display: "flex", flexDirection: "column" }}>
           <div
             style={{
-              flex: 1.5,
-              display: "flex",
-              justifyContent: "space-around",
-              alignItems: "center",
-              flexDirection: "column",
-              borderBottomStyle: "solid",
-              borderBottomColor: "grey",
-              borderBottomWidth: "1px"
+              padding: "3rem"
             }}
           >
-            <Button type="primary">Get Games</Button>
-            {this.state.error && (
-              <Alert
-                type="error"
-                message=""
-                showIcon={true}
-                description={this.state.error}
-                closable={true}
-              />
-            )}
-          </div>
-          <div style={{ flex: 11, display: "flex", flexDirection: "column" }}>
-            <div
-              style={{
-                padding: "3rem"
-              }}
-            >
-              <Table
-                bordered={true}
-                columns={[{ title: "name" }, { title: "location" }]}
-              />
-            </div>
+            <Table
+              bordered={true}
+              columns={[{ title: "name" }, { title: "location" }]}
+            />
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
