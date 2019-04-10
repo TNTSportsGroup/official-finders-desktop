93718293a65005ca8fea50bfa3c46ba1031bcee7
Camilo Guerrero <cami172013@gmail.com>
2019-04-10 10:21:18 -0500

    Added an actual route for QuickScore route

--- src/modules/Routes.tsx
+++ src/modules/Routes.tsx
@@ -4,11 +4,17 @@ import { routes } from "./constants/routes";
 import { Home } from "./Home/Home";
 import { HwrInvoice } from "./HwrInvoice/HwrInvoice";
 import { Hwrp } from "./Hwrp/Hwrp";
+import { QuickScoresGames } from "./QuickScoresGames/QuickScoresGames";
 
 export const Routes = () => (
   <>
-    <Route exac={true} path={routes.HWRPAYROLL} component={Hwrp} />
-    <Route exac={true} path={routes.HWRINVOICE} component={HwrInvoice} />
+    <Route
+      exact={true}
+      path={routes.QUICKSCORESGAMES}
+      component={QuickScoresGames}
+    />
+    <Route exact={true} path={routes.HWRPAYROLL} component={Hwrp} />
+    <Route exact={true} path={routes.HWRINVOICE} component={HwrInvoice} />
     <Route exact={true} path={routes.HOME} component={Home} />
   </>
 );
